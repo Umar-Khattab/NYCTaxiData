e266d6d15f93f24cc068769bce86375f288a6db4
Omar <Omar@DESKTOP-RCI0258>
2026-03-02 23:34:26 +0200

    Add Response Global Contract

---
+++ NYCTaxiData.API/Contracts/APIResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace NYCTaxiData.API.Contracts
+{
+    public class APIResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+        public object? Error { get; set; }
+
+    }
+}
