9f0f2f71ef6393209650992da48e0f9a583fce6e
Omar <Omar@DESKTOP-RCI0258>
2026-04-12 13:32:17 +0200

    "feat(common): add NotFoundException for resource retrieval failures
    - Exception for when requested resource doesn't exist
    - Supports default message, custom message, and inner exception
    - Maps to HTTP 404 Not Found
    - Used by Commands/Queries when entities not found"

--- NYCTaxiData.Application/Common/Exceptions/NotFoundException.cs
+++ NYCTaxiData.Application/Common/Exceptions/NotFoundException.cs
@@ -1,10 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace NYCTaxiData.Application.Common.Exceptions
+﻿namespace NYCTaxiData.Application.Common.Exceptions
 {
-    internal class NotFoundException
+    /// <summary>
+    /// Exception thrown when a requested resource is not found.
+    /// </summary>
+    public class NotFoundException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotFoundException"/> class.
+        /// </summary>
+        public NotFoundException()
+            : base("The requested resource was not found.")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotFoundException"/> class with a message.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        public NotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotFoundException"/> class with a message and inner exception.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="innerException">The inner exception.</param>
+        public NotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
     }
 }
