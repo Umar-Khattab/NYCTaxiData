d3673f9eecac509ec44d8a7b45148d301ad50891
Omar <Omar@DESKTOP-RCI0258>
2026-04-12 13:32:39 +0200

    "feat(common): add ConflictException for business logic validation
    - Exception for invalid state transitions
    - Supports default message, custom message, and inner exception
    - Maps to HTTP 409 Conflict
    - Used when business constraints are violated"

--- NYCTaxiData.Application/Common/Exceptions/ConflictException.cs
+++ NYCTaxiData.Application/Common/Exceptions/ConflictException.cs
@@ -1,10 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace NYCTaxiData.Application.Common.Exceptions
+﻿namespace NYCTaxiData.Application.Common.Exceptions
 {
-    internal class ConflictException
+    /// <summary>
+    /// Exception thrown when there is a conflict in the current state of a resource.
+    /// </summary>
+    public class ConflictException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConflictException"/> class.
+        /// </summary>
+        public ConflictException()
+            : base("A conflict occurred with the current state of the resource.")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConflictException"/> class with a message.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        public ConflictException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConflictException"/> class with a message and inner exception.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="innerException">The inner exception.</param>
+        public ConflictException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
     }
 }
